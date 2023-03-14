5a89576fedf7ffadeba2e2c3b34220348734c973
giripankaj <giripankaj21@gmail.com>
2023-03-14 20:32:24 +0530

    first commit

---
+++ src/dao/tables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author girip
+ */
+public class tables {
+    public static void main(String[] args){
+        try{
+            String userTable="create table user(id int AUTO_INCREMENT primary key,name varchar(200),email varchar(200),mobileNumber varchar(10),address varchar(200),password varchar(200),securityQuestion varchar(200),answer varchar(200),status varchar(20),UNIQUE (email))";
+            String adminDetails="insert into user(name,email,mobileNumber,address,password,securityQuestion,answer,status) values('Admin','admin@gmail.com','1234567890','India','admin','Which primary school do you attended ?','ABC','true')";
+            DbOperations.setDataOrDelete(userTable,"User Table Created Sucessfully");
+            DbOperations.setDataOrDelete(adminDetails,"Admin details added Sucessfully");
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+}
